0a483587b9da6dda958d2346b85b569d2fea0d2c
Andrew Li <liandrew620@gmail.com>
2025-07-15 21:42:46 -0500

    janksquare!! :exploding_head:

--- makeKernel.sh
+++ makeKernel.sh
@@ -1,4 +1,4 @@
-jjc kernel.jank -S -o kernel.s
+jjc kernel.jank -S -o kernel.s -k
 x86_64-elf-gcc -ffreestanding -mcmodel=large -mno-red-zone -mno-mmx -mno-sse -mno-sse2 -c kernel.s -o kernel.o
 x86_64-elf-gcc -ffreestanding -T link.ld kernel.o -o boot/kernel.out -nostdlib
 
