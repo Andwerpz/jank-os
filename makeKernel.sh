afc53c8e0fb9283bde6e62f7565dfa1f60a32a5e
Andrew Li @ Citadel <liandrew620@gmail.com>
2025-07-18 20:51:35 -0500

    added serial (polling) io

--- makeKernel.sh
+++ makeKernel.sh
@@ -7,4 +7,5 @@ x86_64-elf-gcc -ffreestanding -T link.ld kernel.o font.o -o boot/kernel.out -nos
 
 ./mkbootimg config.json myos.img
 # qemu-system-x86_64 myos.img -serial stdio
-qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial stdio myos.img
+# qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial stdio myos.img
+qemu-system-x86_64 -no-reboot -serial stdio myos.img
