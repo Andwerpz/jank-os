d0dc5819c536f3a657bf9da47a4c8e0a2dbdbb5a
Andrew Li <liandrew620@gmail.com>
2025-08-04 20:47:34 -0500

    user code preemption works

--- makeKernel.sh
+++ makeKernel.sh
@@ -13,12 +13,12 @@ x86_64-elf-gcc -ffreestanding -T link.ld kernel.o font.o -o boot/kernel.out -nos
 # qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial stdio myos.img -m 4G
 
 # normal + debug
-qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial stdio myos.img
+# qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial stdio myos.img
 
 # server 
 # qemu-system-x86_64 -no-reboot -serial telnet:localhost:4321,server,nowait myos.img
 
 # server + debug
-# qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial telnet:localhost:4321,server,nowait myos.img
+qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial telnet:localhost:4321,server,nowait myos.img
 
 # telnet localhost 4321
