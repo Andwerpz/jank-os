75a0c0cb7a84c4b0593199ea491174bca6a0f097
Andrew Li <liandrew620@gmail.com>
2025-08-08 00:18:02 -0500

    testing fork

--- makeKernel.sh
+++ makeKernel.sh
@@ -19,15 +19,15 @@ x86_64-elf-gcc -ffreestanding -T link.ld kernel.o font.o -o boot/kernel.out -nos
 # qemu-system-x86_64 -no-reboot -serial telnet:localhost:4321,server,nowait myos.img
 
 # server + debug
-# qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial telnet:localhost:4321,server,nowait -monitor stdio myos.img
+qemu-system-x86_64 -d int,cpu_reset -no-reboot -serial telnet:localhost:4321,server,nowait -monitor stdio myos.img
 
 # telnet localhost 4321
 
 # normal + debug + ahci
-qemu-system-x86_64 \
-    -d int,cpu_reset \
-    -no-reboot \
-    -serial stdio \
-    -device ich9-ahci,id=ahci \
-    -drive file=myos.img,if=none,id=mydisk \
-    -device ide-hd,bus=ahci.0,drive=mydisk
+# qemu-system-x86_64 \
+#     -d int,cpu_reset \
+#     -no-reboot \
+#     -serial stdio \
+#     -device ich9-ahci,id=ahci \
+#     -drive file=myos.img,if=none,id=mydisk \
+#     -device ide-hd,bus=ahci.0,drive=mydisk
