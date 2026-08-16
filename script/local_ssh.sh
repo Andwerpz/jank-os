4c4f1f1cd34256259d2d2bd6f3e958c44e4e5ebf
Andrew Li <liandrew620@gmail.com>
2026-08-16 13:08:07 -0700

    fix stuff

--- script/local_ssh.sh
+++ script/local_ssh.sh
@@ -1,4 +1,6 @@
 # creates a ssh server listening at address 127.0.0.1:2222
+# will need to install 'openssh-server' 
+
 sudo mkdir -p /run/sshd
 sudo ssh-keygen -A
 sudo /usr/sbin/sshd -D -ddd -e -p 2222 -o ListenAddress=127.0.0.1
--- user/home/net/build_tests.sh
+++ user/home/net/build_tests.sh
@@ -2,5 +2,15 @@
 
 for file in *.jank; do
     [[ -e "$file" ]] || continue
-    jjc $file -o ${file%.*}
+
+    cmd=(jjc "$file" --follow-includes -o "${file%.*}")
+    echo "${cmd[*]}"
+
+    output=$("${cmd[@]}" 2>&1)
+    status=$?
+
+    if (( status != 0 )); then
+        printf '%s\n' "$output"
+        exit "$status"
+    fi
 done
